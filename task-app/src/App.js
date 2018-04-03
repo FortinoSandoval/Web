e23927d1535613c052f90d405efa95cbc7b68486
Fortino Sandoval Cortes <contact@fortinosandoval.com>
2018-04-03 10:07:38 -0700

    Add files via upload

---
+++ task-app/src/App.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+// data
+import { tasks } from './tasks.json';
+
+// subcomponents
+import Taskform from './components/TaskForm';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      tasks
+    }
+    this.handleAddTask = this.handleAddTask.bind(this);
+  }
+
+  removeTask(index) {
+    this.setState({
+      tasks: this.state.tasks.filter((e, i) => {
+        return i !== index
+      })
+    });
+  }
+
+  handleAddTask(task) {
+    this.setState({
+      tasks: [...this.state.tasks, task]
+    })
+  }
+
+  render() {
+    const tasks = this.state.tasks.map((task, i) => {
+      return (
+        <div className="col-md-4" key={i}>
+          <div className="card mt-4">
+            <div className="card-title text-center">
+              <h3>{task.title}</h3>
+              <span className="badge badge-pill badge-danger ml-2">
+                {task.priority}
+              </span>
+            </div>
+            <div className="card-body">
+              {task.description}
+            </div>
+            <div className="card-body">
+              {task.responsible}
+            </div>
+            <div className="card-footer">
+              <button
+                className="btn btn-danger"
+                onClick={this.removeTask.bind(this, i)}>
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      )
+    });
+    
+    return (
+      <div className="App">
+
+        <nav className="navbar navbar-dark bg-dark">
+          <a className="navbar-brand" href="/">
+            Tasks
+            <span className="badge badge-pill badge-light ml-2">
+              {this.state.tasks.length}
+            </span>
+          </a>
+        </nav>
+
+        <div className="container">
+          <div className="row mt-4">
+
+            <div className="col-md-4 text-center">
+                <img src={logo} className="App-logo" alt="logo" />
+              <Taskform onAddTodo={this.handleAddTask}></Taskform>
+            </div>
+
+            <div className="col-md-8">
+              <div className="row">
+                {tasks}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
